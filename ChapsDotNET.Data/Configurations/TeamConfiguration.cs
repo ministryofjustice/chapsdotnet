6afc61f79b4f6efcb31dae5f80d9a3b813abb31f
N.Preddy <nick.preddy@digital.justice.gov.uk>
2022-07-04 14:26:24 +0100

    Removed unnecessary line feeds.

--- ChapsDotNET.Data/Configurations/TeamConfiguration.cs
+++ ChapsDotNET.Data/Configurations/TeamConfiguration.cs
@@ -6,15 +6,12 @@ namespace ChapsDotNET.Data.Configurations
 {
     public class TeamConfiguration : IEntityTypeConfiguration<Team>
     {
-
         public void Configure(EntityTypeBuilder<Team> builder)
         {
             builder.HasKey(a => new { a.TeamID });
             builder.Property(e => e.Acronym).IsRequired().HasMaxLength(10);
             builder.Property(e => e.Name).IsRequired().HasMaxLength(40);
             builder.Property(e => e.email).HasMaxLength(80);
-
         }
     }
 }
-
--- ChapsDotNET.Data/Entities/Team.cs
+++ ChapsDotNET.Data/Entities/Team.cs
@@ -4,21 +4,15 @@ namespace ChapsDotNET.Data.Entities;
 
 public class Team : LookUpModel, IAuditable
 {
-
     public int TeamID { get; set; }
-
     public string Acronym { get; set; } = string.Empty;
-
     public string Name { get; set; } = string.Empty;
     //Commented out because we will be using this formatting to define the email.
     //[MaxLength(80), RegularExpression(@"^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-‌​]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$", ErrorMessage = "Email is not valid")]
     public string? email { get; set; } 
-
     public bool isOGD { get; set; }
-
     public bool isPOD { get; set; }
     public virtual ICollection<CorrespondenceTypesByTeam>? CorrespondenceTypes { get; set; }
-
     public virtual ICollection<User>? Users { get; set; }
 
     public Team()
@@ -30,6 +24,4 @@ public class Team : LookUpModel, IAuditable
     {
         return true;
     }
-
-
 }
