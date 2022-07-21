cd0bf6a47a9bdeb21f106408fd2b6b0061ab8a42
N.Preddy <nick.preddy@digital.justice.gov.uk>
2022-07-21 14:08:29 +0100

    Public Holiday Tests

---
+++ ChapsDotNET.Business.Tests/Common/FakePublicHolidaysProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Bogus;  
+using ChapsDotNET.Data.Entities;
+
+namespace ChapsDotNET.Business.Tests.Common
+{
+    public class FakePublicHolidaysProvider
+    {
+        public static Faker<PublicHoliday> FakeData { get; } =
+            new Faker<PublicHoliday>()
+                .RuleFor(x => x.Date, f => f.Date.Between(new DateTime(2022,01,01), new DateTime(2023,01,01)))
+                .RuleFor(x => x.Description, f => f.Hacker.ToString());
+    }
+}
---
+++ ChapsDotNET.Business.Tests/PublicHolidayComponentTests.cs
@@ -0,0 +1,70 @@
+﻿using ChapsDotNET.Business.Components;
+using ChapsDotNET.Business.Exceptions;
+using ChapsDotNET.Business.Models.Common;
+using ChapsDotNET.Business.Tests.Common;
+using ChapsDotNET.Data.Entities;
+using FluentAssertions;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ChapsDotNET.Business.Tests
+{
+    public class PublicHolidayComponentTests
+    {
+        [Fact(DisplayName = "Get a list of public holidays when GetPublicHolidaysAsync is called")]
+        public async Task GetAListOfPublicHolidaysWhenGetPublicHolidaysAsyncIsCalled()
+        {
+            // Arrange
+            var context = DataContextFactory.Create();
+            await context.PublicHolidays.AddAsync(new PublicHoliday{
+                PublicHolidayID = 1,
+                Date = new DateTime(2022,05,04),
+                Description = "May the forth be with you"
+            });
+            await context.SaveChangesAsync();
+
+            var publicHolidayComponent = new PublicHolidayComponent(context);
+
+            // Act
+            var result = await publicHolidayComponent.GetPublicHolidaysAsync(new PublicHolidayRequestModel());
+
+            // Assert
+            result.Results.Should().NotBeNull();
+            result.Results.Should().HaveCount(1);
+            result.Results?.First().Description.Should().Be("May the forth be with you");
+            result.Results?.First().PublicHolidayID.Should().Be(1);
+            result.Results?.First().Date.Should().HaveDay(4);
+            result.Results?.First().Date.Should().HaveMonth(5);
+            result.Results?.First().Date.Should().HaveYear(2022);
+        }
+
+        [Fact(DisplayName = "When we call GetSalutationsAsync we get paged results by default")]
+        public async Task GetPublicHolidaysAsyncReturnsPagedResults()
+        {
+            // Arrange
+            var context = DataContextFactory.Create();
+            var fakePublicHolidays = FakePublicHolidaysProvider.FakeData
+                .Generate(50)
+                .ToList();
+            await context.PublicHolidays.AddRangeAsync(fakePublicHolidays);
+            await context.SaveChangesAsync();
+            var publicHolidaysComponent = new PublicHolidayComponent(context);
+
+            // Act
+            var result = await publicHolidaysComponent.GetPublicHolidaysAsync(new PublicHolidayRequestModel
+            {
+                ShowActiveAndInactive = true
+            });
+
+            // Assert
+            result.PageSize.Should().Be(10);
+            result.PageCount.Should().Be(5);
+            result.RowCount.Should().Be(50);
+            result.CurrentPage.Should().Be(1);
+            result.Results.Should().NotBeNull();
+            result.Results.Should().HaveCount(10);
+        }
+    }
+}
