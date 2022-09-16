9a378f63ac04f951296b5fa68505f05cd3e6ee79
KulwantL <kulwant.lall@digital.justice.gov.uk>
2022-09-16 15:19:52 +0100

    Report model created.

---
+++ ChapsDotNET.Business/Models/ReportModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ChapsDotNET.Business.Models
+{
+    public class ReportModel
+    {
+        public int ReportId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+    }
+}
+
