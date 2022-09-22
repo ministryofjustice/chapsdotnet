a3e66f714b9b49111b674a52d74376dfd67d9d61
KulwantL <kulwant.lall@digital.justice.gov.uk>
2022-09-22 15:09:30 +0100

    Writing of tests. One left for Alistair to complete.

---
+++ ChapsDotNET.Business.Tests/ReportsComponentTests.cs
@@ -0,0 +1,115 @@
+﻿using ChapsDotNET.Business.Components;
+using ChapsDotNET.Business.Exceptions;
+using ChapsDotNET.Business.Models.Common;
+using ChapsDotNET.Business.Tests.Common;
+using ChapsDotNET.Data.Entities;
+using FluentAssertions;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ChapsDotNET.Business.Tests
+{
+    public class ReportsComponentTests
+    {
+        [Fact(DisplayName = "Get a list of Reports when GetReportsAsync is called")]
+        public async Task GetAListOfReportsWhenGetReportsAsyncIsCalled()
+        {
+            // Arrange
+            var context = DataContextFactory.Create();
+            await context.Reports.AddAsync(new Report
+            {
+                ReportId = 1,
+                Name = "Report 1",
+                Description = "Description 1",
+                LongDescription = "Long Description 1"
+            });
+            await context.SaveChangesAsync();
+
+            var ReportComponent = new ReportComponent(context);
+
+            // Act
+            var result = await ReportComponent.GetReportsAsync();
+
+            // Assert
+            result.Should().NotBeNull();
+            result.First().Name.Should().Be("Report 1");
+            result.First().Description.Should().Be("Description 1");
+            result.First().LongDescription.Should().Be("Long Description 1");
+
+        }
+
+        [Fact(DisplayName = "Get a specific Report")]
+        public async Task GetReportAsync()
+        {
+            // Arrange
+            var context = DataContextFactory.Create();
+            await context.Reports.AddAsync(new Report
+            {
+                ReportId = 2,
+                Name = "Report 2",
+                Description = "Description 2",
+                LongDescription = "Long Description 2"
+                
+            });
+
+            await context.Reports.AddAsync(new Report
+            {
+                ReportId = 53,
+                Name = "Report 53",
+                Description = "Description 53",
+                LongDescription = "Long Description 53"
+            });
+
+            await context.SaveChangesAsync();
+
+            var ReportComponent = new ReportComponent(context);
+
+            // Act
+            var result = await ReportComponent.GetReportAsync(53);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.ReportId.Should().Be(53);
+            result.Name.Should().Be("Report 53");
+            result.Description.Should().Be("Description 53");
+            result.LongDescription.Should().Be("Long Description 53");
+        }
+
+        //[Fact(DisplayName = "What happens for the wrong ReportId?")]
+        //public async Task GetWrongReportAsync()
+        //{
+        //    // Arrange
+        //    var context = DataContextFactory.Create();
+        //    await context.Reports.AddAsync(new Report
+        //    {
+        //        ReportId = 2,
+        //        Detail = "Brexit",
+        //        active = true
+        //    });
+
+        //    await context.Reports.AddAsync(new Report
+        //    {
+        //        ReportId = 54,
+        //        Detail = "Courts",
+        //        active = true
+        //    });
+
+        //    await context.SaveChangesAsync();
+
+        //    var ReportComponent = new ReportComponent(context);
+
+        //    // Act
+        //    var result = await ReportComponent.GetReportAsync(53);
+
+        //    // Assert
+        //    result.Detail.Should().BeNull();
+        //}
+
+        
+
+    }
+}
+
+
