aa0a3d6104f8ea0e000ae6b8763107af417c854c
N.Preddy <nick.preddy@digital.justice.gov.uk>
2022-12-07 16:28:31 +0000

    wip 07.12.2022 16.28

--- ChapsDotNET.Business/Models/Common/PagedResult.cs
+++ ChapsDotNET.Business/Models/Common/PagedResult.cs
@@ -7,10 +7,10 @@
         public int PageCount => (int)Math.Ceiling((double)RowCount / PageSize);
         public int PageSize { get; set; }
         public int RowCount { get; set; }
-        public string? sortOrder { get; set; }
-        public string? nameFilterTerm { get; set; }
-        public string? addressFilterTerm { get; set; }
-        public string? emailFilterTerm { get; set; }
+        //public string? sortOrder { get; set; }
+        //public string? nameFilterTerm { get; set; }
+        //public string? addressFilterTerm { get; set; }
+        //public string? emailFilterTerm { get; set; }
         public bool activeFilter { get; set; }
     }
 
