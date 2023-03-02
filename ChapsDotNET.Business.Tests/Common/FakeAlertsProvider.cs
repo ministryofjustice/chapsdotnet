1f632d052e707eb4d2c0cf6a5b4372f583eb1e6d
danielatdigital <danielatdigital@users.noreply.github.com>
2023-03-02 15:56:45 +0000

    Remove unwanted properties

--- ChapsDotNET.Business.Tests/Common/FakeAlertsProvider.cs
+++ ChapsDotNET.Business.Tests/Common/FakeAlertsProvider.cs
@@ -8,8 +8,6 @@ namespace ChapsDotNET.Business.Tests.Common
         public static Faker<Alert> FakeData { get; } =
             new Faker<Alert>()
                 .RuleFor(x => x.Message, f => f.Lorem.Word())
-                .RuleFor(x => x.live, f => f.Random.Bool())
-                .RuleFor(x => x.deactivated, f => f.Date.Past())
-                .RuleFor(x => x.deactivatedBy, f => f.Name.FirstName());
+                .RuleFor(x => x.live, f => f.Random.Bool());
     }
 }
