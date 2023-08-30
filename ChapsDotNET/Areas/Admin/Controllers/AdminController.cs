f5143f2b17fa87df63d8e06e1401321b5a56974f
ayeshamoj <ayesha.kholwadia@digital.justice.gov.uk>
2023-08-30 15:26:03 +0100

    Added new admin page and relevant files

---
+++ ChapsDotNET/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Chaps.Areas.Admin.Controllers
+{
+    ////[ChapsRedirect]
+    //[AuthorizeRedirect(MinimumRequiredAccessLevel = AccessLevel.Manager)]
+    //[Authorize]
+    //[ValidateAntiForgeryTokenOnAllPosts]
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/Admin/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Lookups()
+        {
+            return View();
+        }
+
+    }
+}
+
+
