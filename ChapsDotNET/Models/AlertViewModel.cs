ab7d8c1c8e7e39ee9e176ab5e0b406f714e76583
alistair.curtis <Alistair.Curtis@justice.gov.uk>
2023-09-26 15:21:32 +0100

    styles for Users, Alerts

--- ChapsDotNET/Models/AlertViewModel.cs
+++ ChapsDotNET/Models/AlertViewModel.cs
@@ -14,13 +14,16 @@ namespace ChapsDotNET.Models
 		public bool Live { get; set; }
 		[Required]
 		public DateTime EventStart { get; set; }
+
 		public int RaisedHours { get; set; }
 		[Required]
 		public DateTime WarnStart { get; set; }
 		[Required, MaxLength(200)]
 		public string? Message { get; set; }
 
+		[Required]
 		public string? WarnStartString { get; set; }
+		[Required]
         public string? EventStartString { get; set; }
     }
 }
