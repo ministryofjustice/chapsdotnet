ef8e058f918115b9d79c9b1395aee50d6c98ba90
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2024-11-29 16:48:35 +0000

    refactor

--- ChapsDotNET/Common/DynamicProxy.cs
+++ ChapsDotNET/Common/DynamicProxy.cs
@@ -1,9 +1,4 @@
-using System.Net.Http;
 using System.Text.Json;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Yarp.ReverseProxy.Configuration;
-
 
 namespace ChapsDotNET.Common
 {
@@ -14,45 +9,43 @@ namespace ChapsDotNET.Common
             // Get ECS metadata endpoint
             var metadataEndpoint = Environment.GetEnvironmentVariable("ECS_CONTAINER_METADATA_URI_V4");
             Console.WriteLine($"Metadata Endpoint: {metadataEndpoint}");
+            string? ipAddress = string.Empty;
+            if (!string.IsNullOrEmpty(metadataEndpoint))
+            {
 
-            if (string.IsNullOrEmpty(metadataEndpoint))
+                // Fetch metadata from ECS
+                try
+                {
+                    using (var client = new HttpClient())
+                    {
+                        var response = await client.GetStringAsync(metadataEndpoint);
+                        var metadata = JsonDocument.Parse(response);
+                        Console.WriteLine($"Metadata: {metadata}");
+
+                        ipAddress = metadata.RootElement
+                            .GetProperty("Networks")[0]
+                            .GetProperty("IPv4Addresses")[0]
+                            .GetString();
+
+                        if (!string.IsNullOrEmpty(ipAddress))
+                        {
+                            Console.WriteLine($"IP address: {ipAddress}");
+                            return ipAddress;
+                        }
+                    }
+                }
+                catch (HttpRequestException ex)
+                {
+                    Console.WriteLine($"Failed to retrieve metadata: {ex.Message}");
+                }
+            }
+            else
             {
                 Console.WriteLine("Metadata endpoint is not available.");
                 throw new InvalidOperationException("Metadata endpoint is not available.");
             }
-
-            string? ipAddress;
-
-            // Fetch metadata from ECS
-            try
-            {
-                using (var client = new HttpClient())
-                {
-                    var response = await client.GetStringAsync(metadataEndpoint);
-                    var metadata = JsonDocument.Parse(response);
-                    Console.WriteLine($"Metadata: {metadata}");
-
-                    ipAddress = metadata.RootElement
-                        .GetProperty("Networks")[0]
-                        .GetProperty("IPv4Addresses")[0]
-                        .GetString();
-                
-                    Console.WriteLine($"IP address: {ipAddress}");
-                }
-            }
-            catch (HttpRequestException ex)
-            {
-                Console.WriteLine($"Failed to retrieve metadata: {ex.Message}");
-                throw;
-            }
-
-            if (string.IsNullOrEmpty(ipAddress))
-            {
-                throw new InvalidOperationException("Could not determine IP address from metadata.");
-            }
-            Console.WriteLine($"IP address: {ipAddress}");
+    
             return ipAddress;
-
         }
     }
 }
