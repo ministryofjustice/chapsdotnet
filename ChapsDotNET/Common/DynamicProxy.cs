56a25e4466a8ba365bf58a76962218c7885d5364
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2024-12-02 13:22:10 +0000

    further debugging ECS_URI endpoint

--- ChapsDotNET/Common/DynamicProxy.cs
+++ ChapsDotNET/Common/DynamicProxy.cs
@@ -7,7 +7,7 @@ namespace ChapsDotNET.Common
         public async Task<string> GetContainerIpAddressAsync()
         {
             // Get ECS metadata endpoint
-            var metadataEndpoint = Environment.GetEnvironmentVariable("ECS_CONTAINER_METADATA_URI_V4");
+            var metadataEndpoint = "http://169.254.170.2/v4/a7199647-384e-40b9-a81a-dd17d6f7e2d8"; //Environment.GetEnvironmentVariable("ECS_CONTAINER_METADATA_URI_V4");
             if (string.IsNullOrEmpty(metadataEndpoint))
             {
                 throw new InvalidOperationException("Metadata endpoint is not available.");
@@ -21,7 +21,6 @@ namespace ChapsDotNET.Common
             
             string? ipAddress = string.Empty;
 
-
             Console.WriteLine($"Metadata Endpoint: {metadataEndpoint}");
 
             // Fetch metadata from ECS
@@ -29,27 +28,37 @@ namespace ChapsDotNET.Common
             {
                 try
                 {
+                    Console.WriteLine($"Attempting to fetch metadata from: {metadataEndpoint}");
+                    Console.WriteLine($"HttpClient BaseAddress: {client.BaseAddress}");
+                    Console.WriteLine($"HttpClient DefaultRequestHeaders: {string.Join(", ", client.DefaultRequestHeaders)}");
+
+                    var response = await client.GetAsync(metadataEndpoint);
+                    
+                    Console.WriteLine($"Response Status Code: {response.StatusCode}");
+                    Console.WriteLine($"Response Headers: {string.Join(", ", response.Headers.Select(h => $"{h.Key}: {string.Join(", ", h.Value)}"))}");
+
+                    response.EnsureSuccessStatusCode();
+                    
+                    var content = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine($"Response Content: {content}");
+                    
+                    var metadata = JsonDocument.Parse(content);
+                    
+
+                    if (!metadata.RootElement.TryGetProperty("Networks", out var networks))
                     {
-                        Console.WriteLine($"Attempting to fetch metadata from: {metadataEndpoint}");
-                        var response = await client.GetStringAsync(metadataEndpoint);
-                        Console.WriteLine($"Metadata: {response}");
-                        var metadata = JsonDocument.Parse(response);
-
-                        if (!metadata.RootElement.TryGetProperty("Networks", out var networks))
-                        {
-                            throw new InvalidOperationException("Metadata does not contain 'Networks'.");
-                        }
-                        
-                        ipAddress = networks[0].GetProperty("IPv4Addresses")[0].GetString();
-                        
-                        if (string.IsNullOrEmpty(ipAddress))
-                        {
-                            throw new InvalidOperationException("Failed to retrieve valid IP address from metadata.");
-                        }
-
-                        Console.WriteLine($"Extracted IP address: {ipAddress}");
-                        return ipAddress;
+                        throw new InvalidOperationException("Metadata does not contain 'Networks'.");
                     }
+                    
+                    ipAddress = networks[0].GetProperty("IPv4Addresses")[0].GetString();
+                    
+                    if (string.IsNullOrEmpty(ipAddress))
+                    {
+                        throw new InvalidOperationException("Failed to retrieve valid IP address from metadata.");
+                    }
+
+                    Console.WriteLine($"Extracted IP address: {ipAddress}");
+                    return ipAddress;
                 }
                 catch (Exception ex)
                 {
