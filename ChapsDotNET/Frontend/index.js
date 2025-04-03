df49c86ee81bb98a45f29b98c1b40a3c2b776623
EmilyHazlehurst <emily.hazlehurst@digital.justice.gov.uk>
2025-04-03 17:09:51 +0100

    Initial frontend improvements - Add webpack config to build js/css into wwwroot - Add package.json - Import base moj and govuk styles and set up initial frontend structure

---
+++ ChapsDotNET/Frontend/index.js
@@ -0,0 +1,10 @@
+// Base styles
+import './base.scss';
+
+// JavaScript
+import './Components/Alert/Alert.js';
+import './Components/Breadcrumbs/Breadcrumbs.js';
+import './Components/Footer/Footer.js';
+import './Components/Header/Header.js';
+import './Components/PrimaryNavigation/PrimaryNavigation.js';
+import './Components/IdentityBar/IdentityBar.js';
