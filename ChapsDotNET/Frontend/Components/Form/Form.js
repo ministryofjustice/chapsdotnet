6127e8ee93a4f4bbf9924e8a11101e455445201b
EmilyHazlehurst <emily.hazlehurst@digital.justice.gov.uk>
2025-04-28 13:07:18 +0100

    - Fix address in helpers - Update Create/Edit templates - Add UserForm - Add JS to handle form errors

---
+++ ChapsDotNET/Frontend/Components/Form/Form.js
@@ -0,0 +1,40 @@
+﻿import './Form.scss';
+
+export default function () {
+    const form = document.querySelector(".chaps-form");
+  
+    if (!form) {
+        return;
+    }
+
+    const observer = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+            if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+                const el = mutation.target;
+                const group = el.closest('.govuk-form-group');
+                const input = group.querySelector('input, select')
+                if (!group && !input) {
+                    return;
+                }
+                if (el.classList.contains('field-validation-error')) {
+                    group.classList.add("govuk-form-group--error");
+                    input.classList.add("govuk-input--error");
+                }
+                else if (el.classList.contains('field-validation-valid')) {
+                    group.classList.remove("govuk-form-group--error");
+                    input.classList.remove("govuk-input--error");
+                }
+            }
+        });
+    });
+
+    const formGroups = form.querySelectorAll(".govuk-form-group");
+
+    formGroups.forEach((group) => {
+        const errorPlaceholder = group.querySelector(".field-validation-valid");
+        observer.observe(errorPlaceholder, {
+            attributes: true,
+            attributeFilter: ['class']
+        });
+    });
+}
--- ChapsDotNET/Models/UserViewModel.cs
+++ ChapsDotNET/Models/UserViewModel.cs
@@ -4,23 +4,25 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ChapsDotNET.Models
 {
-	public class UserViewModel
-	{
+    public class UserViewModel
+    {
         [Key]
         public int UserId { get; set; }
-        [Required, MaxLength(50)]
+        [Required(ErrorMessage = "Enter a name"), MaxLength(50)]
         public string? Name { get; set; }
-        [Required, MaxLength(50)]
+        [Required(ErrorMessage = "Enter a display name"), MaxLength(50)]
         public string? DisplayName { get; set; }
 
-        [MaxLength(80), RegularExpression(@"^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-‌​]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$", ErrorMessage = "Email is not valid")]
+        [MaxLength(80), RegularExpression(@"^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-‌​]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$", ErrorMessage = "Enter an email address in the correct format, like name@example.com")]
         public string? Email { get; set; }
 
+        [Required(ErrorMessage = "Select a team")]
         public int TeamId { get; set; }
         public Team? Team { get; set; }
 
         public int RoleId { get; set; }
         public Role? Role { get; set; }
+        [Required(ErrorMessage = "Select a role")]
         public int RoleStrength { get; set; }
 
         public SelectList? TeamList { get; set; }
